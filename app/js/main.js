a3357ad9ecbd42509f47cf1d50bf57e5372f246b
David Vodzinskyi <nikeron2002@gmail.com>
2022-02-02 13:25:59 +0200

    added breadcrumbs

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,17 @@
+
 $ (function(){
    $('.slider-top__items').slick({
       arrows: false,
       dots:true,
       fade:true,
      });
+
+     var mixer = mixitup('.products__all');
+     var mixer = mixitup('.design__all');
+
+    
+
+
    //   $('').rateyo({
    //    // rating: 3.6,onlyjs
    //    // html data-rateyo-rating="3.6"
