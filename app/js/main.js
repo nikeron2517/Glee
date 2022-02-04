effcf369f35564135d91c12a07e7daa9229018a4
David Vodzinskyi <nikeron2002@gmail.com>
2022-02-04 20:59:53 +0200

    added filter

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,35 @@
 
 $ (function(){
+
+   $('.filter-price__input').ionRangeSlider({
+        type: "double",
+        prefix: "$",
+      //   <input type="text"
+      //   data-min="0">
+      //   data-max="999"
+      //   data-from="200"
+      //   data-to="500"
+      onStart: function (data) {
+         $('.filter-price__from').text(data.from);
+         $('.filter-price__to').text(data.to);
+      },
+      onChange: function (data) {
+         $('.filter-price__from').text(data.from);
+         $('.filter-price__to').text(data.to);
+     },
+     
+   });
+
+   $('.filter-recent__star').rateYo({
+      rating: 3.6, //onlyjs
+      // html data-rateyo-rating="3.6" 
+      spacing: "8px",
+     starWidth: "11px",
+     normalFill: "#d6d6d6",
+     ratedFill: "#ffcc00",
+     readOnly: true //disabled changing
+    });
+
    $('.slider-top__items').slick({
       arrows: false,
       dots:true,
@@ -9,15 +39,9 @@ $ (function(){
      var mixer = mixitup('.products__all');
      var mixer = mixitup('.design__all');
 
+      
+     
     
 
 
-   //   $('').rateyo({
-   //    // rating: 3.6,onlyjs
-   //    // html data-rateyo-rating="3.6"
-   //    starWidth: "17px",
-   //    normalFill: "#",
-   //    ratedFill: "#",
-   //    readOnly: true //disabled changing
-   //   });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,7 @@ function scripts(){
      'node_modules/rateyo/src/jquery.rateyo.js',
      'node_modules/mixitup/dist/mixitup.js',
      'node_modules/jquery/dist/jquery.js',
+     'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
      'app/js/main.js'
    ])
    .pipe(concat('main.min.js'))
